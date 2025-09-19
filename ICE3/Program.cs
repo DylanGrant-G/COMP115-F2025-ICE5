4481dbbf9842844596822f5ae6151e93fd5eb2e8
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2025-09-19 08:24:31 -0400

    initial commit

---
+++ ICE3/Program.cs
@@ -0,0 +1,34 @@
+﻿// copied from https://github.com/GeorgianCollege/COMP1115-F2025-ICE2
+
+namespace ICE3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.Title = "Character Sheet";
+
+            string CharacterName;
+
+            // Primary Attributes
+            int Agility;
+            int Strength;
+            int Vigour;
+            int Perception;
+            int Intellect;
+            int Will;
+
+            // Secondary Attributes
+            int Awareness;
+            int Toughness;
+            int Resolve;
+
+            // Prompt for CharacterName
+            Console.Write("Enter Character Name: ");
+            CharacterName = Console.ReadLine();
+        }
+    }
+}
